8baa6a5031dc801f831be0e10e246e0c68f519fe
Yash10 <72209006+ninjayash@users.noreply.github.com>
2021-10-01 11:47:11 +0530

    Update app.py

--- Python/Internet Speed Checker/app.py
+++ Python/Internet Speed Checker/app.py
@@ -1,5 +1,6 @@
 # pip install speedtest-cli
 # Do not use pip install speedtest
+#use pip install speedtest-cli ,happy coding
 import speedtest
 wifi  = speedtest.Speedtest()
 print("Internet Download Speed is", wifi.download(),"bit")
